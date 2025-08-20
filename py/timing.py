cdf8a770cea797914c18759ead93cee8fec5ebc4
Enigmatisms <heqianyue@baidu.com>
2025-08-20 12:53:07 +0000

    [Minor] Add optimization options in CMakeLists.txt

--- py/timing.py
+++ py/timing.py
@@ -5,7 +5,7 @@ def test_time(module_name = "fabular"):
     num_test = 50
     exclude_first = 1
     exclude_last = 1
-    repeat = 20000 + exclude_first + exclude_last
+    repeat = 200000 + exclude_first + exclude_last
     execution_times = timeit.repeat(
         "a = fbl.process_dlpack(dl_cuda)",
         setup=f"import torch; import {module_name} as fbl; cuda_tensor = torch.tensor([[1.1, 2.2], [3.3, 4.4]], device='cuda'); dl_cuda = cuda_tensor.__dlpack__()",
