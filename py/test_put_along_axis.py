7c24fb5065657afeec8dda127a9907af116ba654
Enigmatisms <heqianyue@baidu.com>
2025-08-22 03:23:20 +0000

    [Feat] Fabular add a new kernel and tested: put_along_axis

--- py/test_put_along_axis.py
+++ py/test_put_along_axis.py
@@ -6,17 +6,64 @@ import numpy as np
 import torch
 import paddle
 
-if __name__ == "__main__":
-    inout_tensor = torch.zeros(2, 3, 4, 5).cuda()
-    gt_tensor = inout_tensor.clone()
-    indices = torch.randint(0, 3, (2, 2, 4, 5), dtype=torch.int64).cuda()
-    dim = 2
+def try_test(actual_np: np.ndarray, gt_np: np.ndarray, actual_name: str):
+    try:
+        np.testing.assert_allclose(actual_np, gt_np)
+    except AssertionError as ex_msg:
+        print(f"{actual_name} put_along_axis differs from PyTorch!")
+        print(f"{actual_name}:")
+        print(actual_np)
+        print("PyTorch:")
+        print(gt_np)
+        print(ex_msg)
 
-    result = torch.scatter(gt_tensor, dim, indices, 6)
-    print("Result gt:\n", result.cpu().numpy())
+def make_case_and_test(input_shape, index_shape, dim, indices_range, value_to_set = 6):
+    inout_tensor = torch.zeros(*input_shape, dtype=torch.float32).cuda()
+    gt_tensor = inout_tensor.clone()
+    indices = torch.randint(0, indices_range, index_shape, dtype=torch.int64).cuda()
+
+    result = torch.scatter(gt_tensor, dim, indices, value_to_set)
+    pd_input = paddle.zeros(*input_shape, dtype=paddle.float32)
+    pd_indices = paddle.to_tensor(indices.cpu().numpy())
+    pd_result = paddle.put_along_axis(pd_input, indices = pd_indices, values = value_to_set, axis = dim, broadcast = False)
 
     dl_inout = inout_tensor.__dlpack__()
     dl_indices = indices.__dlpack__()
     fbl.put_along_axis(dl_inout, dl_indices, dim)
-    print("Result mine:\n", inout_tensor.cpu().numpy())
-    np.testing.assert_allclose(inout_tensor.cpu().numpy(), result.cpu().numpy())
+
+    try_test(inout_tensor.cpu().numpy(), result.cpu().numpy(), "Fabular")
+    try_test(pd_result.numpy(), result.cpu().numpy(), "Paddle")
+    
+
+if __name__ == "__main__":
+    torch.manual_seed(114514)
+    inout_tensor = torch.zeros(2, 3, 4, 5).cuda()
+
+    print("Case 1:")
+    make_case_and_test((2, 3, 4, 5), (2, 2, 4, 5), dim = 2, indices_range = 3)
+    print("Case 2:")
+    make_case_and_test((2, 3, 4, 5), (2, 1, 4, 5), dim = 3, indices_range = 5)
+    print("Case 3:")
+    make_case_and_test((2, 2, 3, 2, 5), (2, 2, 2, 2, 5), dim = 3, indices_range = 2)
+    print("Case 4:")
+    make_case_and_test((2, 5, 2, 3, 2), (2, 5, 2, 2, 2), dim = 1, indices_range = 5)
+
+    print("\nExpect same cases:\n")
+    print("Case 5:")
+    make_case_and_test((8, 8, 8), (7, 8, 8), dim = 1, indices_range = 8)
+
+    print("Case 6:")
+    make_case_and_test((257, 63, 128), (257, 63, 128), dim = 0, indices_range = 128)
+
+    print("Case 7:")
+    make_case_and_test((37, 18), (31, 16), dim = 0, indices_range = 20)
+
+    print("Case 8:")
+    make_case_and_test((129, 2), (16, 2), dim = 0, indices_range = 64)
+
+    print("Case 9:")
+    make_case_and_test((1023,), (1023,), dim = 0, indices_range = 512)
+
+    print("Case 10:")
+    make_case_and_test((1023,), (167,), dim = 0, indices_range = 512)
+    
--- src/put_along_axis.cu
+++ src/put_along_axis.cu
@@ -1,60 +1,45 @@
 /**
  * A simple test module for put along axis (isolated test)
+ * Note that, I don't want to optimize this any more, since
+ * the implementation is migrated to paddle's PHI lib
+ * 
+ * I will optimize things there.
 */
 #include <cstdint>
 #include <iostream>
 #include <cuda_runtime.h>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
 #include "cuda_utils.cuh"
 #include "unified_tensor.hpp"
 
-constexpr int MAX_DIMS = 9;
-
 namespace fab {
-
-struct TensorDesc {
-    /**
-     * Basic version, no fast div mod, just simple mod and div
-     * We can optimize this later
-     * 
-     * We can use only 16 int64_t in total!
-     * 1. dim to put does not need to be recorded here
-     * 2. the last stride (well, maybe we need to record it, since transpose can happen, ask somebody who's good at this)
-    */
-    int64_t shape[MAX_DIMS];            // shape of the index tensor
-    int64_t stride_in[MAX_DIMS];        // stride of the input tensor
-};
-
-TensorDesc* copy_desc_to_device(const TensorDesc& host_desc) {
-    TensorDesc* d_desc;
-    CUDA_CHECK_RETURN(cudaMalloc(&d_desc, sizeof(TensorDesc)));
-    CUDA_CHECK_RETURN(cudaMemcpy(d_desc, &host_desc, sizeof(TensorDesc), cudaMemcpyHostToDevice));
-    return d_desc;
-}
 
 __device__ __forceinline__
 int64_t compute_offset(
-    const TensorDesc* desc, 
+    const int64_t* index_shape,
     int64_t tid, 
     const int ndim, 
     const int dim_to_put,
     const int64_t idx_on_dim
 ) {
     int64_t offset = 0;
-
-    // TODO(heqianyue): can be optimized by faster div mod
-    // TODO(heqianyue): (builtin assume) shape > 0, no valid check?
+    // index shape tensor is reused, the first `ndim` elements are the shape of the index tensor
+    // while the second `ndim` elements are the strides of the input tensor
+    const int64_t* input_stride = index_shape + ndim;
     for (int d = ndim - 1; d > dim_to_put; --d) {
         // before the put dim
-        int64_t this_shape = desc->shape[d];
-        offset += (tid % this_shape) * desc->stride_in[d];      
+        int64_t this_shape = index_shape[d];
+        offset += (tid % this_shape) * input_stride[d];      
+
         tid /= this_shape;
     }
-    offset += idx_on_dim * desc->stride_in[dim_to_put];
-    tid /= desc->shape[dim_to_put];
+    offset += idx_on_dim * input_stride[dim_to_put];
+    tid /= index_shape[dim_to_put];
     for (int d = dim_to_put - 1; d >= 0; --d) {
         // after the put dim
-        int64_t this_shape = desc->shape[d];
-        offset += (tid % this_shape) * desc->stride_in[d];      
+        int64_t this_shape = index_shape[d];
+        offset += (tid % this_shape) * input_stride[d];      
         tid /= this_shape;
     }
     return offset;
@@ -64,24 +49,24 @@ template <typename T>
 __global__ void ScatterKernel(
     T* inout_tensor,
     const int64_t* indices, 
-    const TensorDesc* desc, 
+    const int64_t* desc, 
     const int dim,
     const int ndim,
     const int64_t numel,
     T value_to_set
 ) {
-    __shared__ TensorDesc smem_desc;
-    if (threadIdx.x < (sizeof(TensorDesc) >> 3)) {
+    extern __shared__ int64_t smem_desc[];
+    if (threadIdx.x < (2 * ndim)) {
         *(reinterpret_cast<int64_t*>(&smem_desc) + threadIdx.x) = 
-        *(reinterpret_cast<int64_t*>(&desc) + threadIdx.x);
+        *(reinterpret_cast<const int64_t*>(desc) + threadIdx.x);
     }
     const int64_t thread_num = gridDim.x * blockDim.x;
     __syncthreads();
     int64_t flattened_idx = threadIdx.x + blockDim.x * blockIdx.x;
     // thread coarsening, which is necessary
     for (int64_t idx = flattened_idx; idx < numel; idx += thread_num) {
-        int64_t idx_on_dim = indices[flattened_idx];
-        int64_t inout_index = compute_offset(&smem_desc, flattened_idx, ndim, dim, idx_on_dim);
+        int64_t idx_on_dim = indices[idx];
+        int64_t inout_index = compute_offset(smem_desc, idx, ndim, dim, idx_on_dim);
         // for actual put along axis, replace the following with src tensor
         atomicExch(inout_tensor + inout_index, value_to_set);
     }
@@ -98,13 +83,16 @@ void TensorProcessor::put_along_axis(DLManagedTensor* inout_m, const DLManagedTe
 
     int64_t numel_inds = indices.strides[0] * indices.shape[0];
 
-    TensorDesc desc;
+   thrust::host_vector<int64_t> host_vec(indices.ndim * 2);
+   thrust::device_vector<int64_t> devc_vec(indices.ndim * 2);
+
     for (int i = 0; i < indices.ndim; i++) {
-        desc.shape[i] = inout.shape[i];
-        desc.stride_in[i] = inout.strides[i];
+        host_vec[i] = indices.shape[i];
+        host_vec[i + indices.ndim] = inout.strides[i];
     }
     
-    auto dev_desc = copy_desc_to_device(desc);
+    devc_vec = host_vec;
+    const int64_t* dev_desc = thrust::raw_pointer_cast(devc_vec.data());
 
     constexpr int64_t thread_num = 128;
     constexpr int64_t max_thread_num = 2147483647;      // INT_MAX, actually, this should be device-aware
@@ -113,11 +101,12 @@ void TensorProcessor::put_along_axis(DLManagedTensor* inout_m, const DLManagedTe
     if (inout.dtype.bits != 32) {
         THROW_IN_HOST("Unsupported data type with bits = %d.\n", inout.dtype.bits);
     }
-#define LAUNCH_SCATTER_KERNEL(dtype)                                \
-    ScatterKernel<dtype><<< block_num, thread_num >>>(              \
-        static_cast<dtype*>(inout.data),                            \
-        static_cast<const int64_t*>(indices.data),                  \
-        dev_desc, dim, indices.ndim, numel_inds, 6                  \
+    const size_t dyn_smem_size = sizeof(int64_t) * indices.ndim * 2;       
+#define LAUNCH_SCATTER_KERNEL(dtype)                                    \
+    ScatterKernel<dtype><<< block_num, thread_num,  dyn_smem_size>>>(   \
+        static_cast<dtype*>(inout.data),                                \
+        static_cast<const int64_t*>(indices.data),                      \
+        dev_desc, dim, indices.ndim, numel_inds, 6                      \
     )
     if (inout.dtype.code == kDLFloat) {
         LAUNCH_SCATTER_KERNEL(float);
@@ -127,7 +116,6 @@ void TensorProcessor::put_along_axis(DLManagedTensor* inout_m, const DLManagedTe
         THROW_IN_HOST("Unsupported data type.");
     }
 #undef LAUNCH_SCATTER_KERNEL
-    CUDA_CHECK_RETURN(cudaFree(dev_desc));
 }
 
 
